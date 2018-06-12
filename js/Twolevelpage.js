cd75817632c698ba28b4ad74ab55d46f8f8a769b
sun <330670679@qq.com>
2018-06-12 21:37:51 +0800

    新增产品列表页

--- js/Twolevelpage.js
+++ js/Twolevelpage.js
@@ -27,3 +27,149 @@ $(function(){
 	})
 })
 
+//更多与收起动效
+$(function(){
+	$("#clickOne").toggle(
+		function(){
+			$("#showMainOne").css("display","block");
+			$("#clickOne p:first-child").html("收起");
+			$("#clickOne p:last-child").css({"width":"0",
+	        "height":"0",
+	        "border":"5px solid transparent",
+	        "border-top-color":"#818286;"}.html());
+		},
+		function(){
+			$("#showMainOne").css("display","none");
+			$("#clickOne p:first-child").html("更多");
+			$("#showMainOne").css("display","block");
+			$("#clickOne p:first-child").html("收起");
+		}
+	)
+	
+})
+
+
+//滑过产品显示缩略图
+$(function(){
+	$('#oneShow').on('mouseover', function() {
+		$("#oneShow").css("visibility","hidden");
+		$("#oneHide").css({"visibility":"visible","z-index":"100"});
+		$("#oneHide").css("box-shadow","0 0 10px gray");
+		
+	})
+	$("#oneHide").on('mouseleave', function() {
+		$("#oneHide").css("visibility","hidden");
+		$("#oneShow").css("visibility","visible");
+	})
+	
+	$("#twoShow").hover(
+		function() {
+			$(this).css("box-shadow","0 0 10px gray");
+		},
+		function(){
+			$(this).css("box-shadow","none");
+		}
+	)
+
+	
+	
+	
+	$("#onePic img:nth-child(1)").mouseenter(function(){
+		$("#onePic img:nth-child(1)").css("border","4px solid #c69a62");
+		$("#onePic img:nth-child(2)").css("border","4px solid #fff");
+		$("#onePic img:nth-child(3)").css("border","4px solid #fff");
+		$("#onepicTit img").attr("src","img/sousuopic2.png");
+	})
+	$("#onePic img:nth-child(2)").mouseenter(function(){
+		$("#onePic img:nth-child(2)").css("border","4px solid #c69a62");
+		$("#onePic img:nth-child(1)").css("border","4px solid #fff");
+		$("#onePic img:nth-child(3)").css("border","4px solid #fff");
+		$("#onepicTit img").attr("src","img/sousuopic1.png");
+	})
+	$("#onePic img:nth-child(3)").mouseenter(function(){
+		$("#onePic img:nth-child(3)").css("border","4px solid #c69a62");
+		$("#onePic img:nth-child(2)").css("border","4px solid #fff");
+		$("#onePic img:nth-child(1)").css("border","4px solid #fff");
+		$("#onepicTit img").attr("src","img/sousuopic3.png");
+	})
+})
+
+
+//购物车固定定位
+$(function(){
+	$(".shoppinguser").hover(
+		function(){
+			$(".shoppinguser").css("background","#fc3f83");
+			$(".shoppinguser i").css("color","#fff");
+			$(".shoppinguser img").fadeIn("slow");
+		},
+		function(){
+			$(".shoppinguser").css("background","#4c4c4c");
+			$(".shoppinguser i").css("color","#d08c35");
+			$(".shoppinguser img").css("display","none");
+		}
+	);
+	$(".shoppingcar").hover(
+		function(){
+			$(".shoppingcar").css("background","#fc3f83");
+			$(".shoppingcar i").css("color","#fff");
+		},
+		function(){
+			$(".shoppingcar").css("background","#4c4c4c");
+			$(".shoppingcar i").css("color","#d08c35");
+		}
+	);
+	$(".shoppingzan").hover(
+		function(){
+			$(".shoppingzan").css("background","#fc3f83");
+			$(".shoppingzan i").css("color","#fff");
+			$(".shoppingzan img").fadeIn("slow");
+		},
+		function(){
+			$(".shoppingzan").css("background","#4c4c4c");
+			$(".shoppingzan i").css("color","#d08c35");
+			$(".shoppingzan img").css("display","none");
+		}
+	);
+	$(".shoppinghistory").hover(
+		function(){
+			$(".shoppinghistory").css("background","#fc3f83");
+			$(".shoppinghistory i").css("color","#fff");
+			$(".shoppinghistory img").fadeIn("slow");
+		},
+		function(){
+			$(".shoppinghistory").css("background","#4c4c4c");
+			$(".shoppinghistory img").css("display","none");
+			$(".shoppinghistory i").css("color","#d08c35");
+		}
+	);
+	$(".shoppingservice").hover(
+		function(){
+			$(".shoppingservice").css("background","#fc3f83");
+			$(".shoppingservice i").css("color","#fff");
+			$(".shoppingservice img").fadeIn("slow");
+		},
+		function(){
+			$(".shoppingservice").css("background","#4c4c4c");
+			$(".shoppingservice img").css("display","none");
+			$(".shoppingservice i").css("color","#d08c35");
+		}
+	);
+	$(".shoppingerweima").hover(
+		function(){
+			$(".shoppingerweima").css("background","#fc3f83");
+			$(".shoppingerweima i").css("color","#fff");
+		},
+		function(){
+			$(".shoppingerweima").css("background","#4c4c4c");
+			$(".shoppingerweima i").css("color","#d08c35");
+		}
+	);
+	$(window).scroll(function(){
+		if($(window).scrollTop != 0){
+			$(".shoppingtop").css("display","block");
+		}else{
+			$(".shoppingtop").css("display","none");
+		}
+	});
+})
--- php/savemesg.php
+++ php/savemesg.php
@@ -1,43 +1,19 @@
+
 <?php
-	header("Content-type","text/html;charset=utf-8");
-	//1、输入（接受前端的数据）
+	header("content-type","text/html;charset=utf-8");
+	// 1、接收前端数据
 	$username = $_POST['username'];
 	$userpassword = $_POST['userpassword'];
 	$useremail = $_POST['useremail'];
-
-    //2、处理（连接数据库，保存数据）
-    $resultStr="";
-    //1)、连接数据库（搭桥）
-    $conn = mysql_connect("localhost","root","root");
-    if(!$conn){
-    	//die("Could not connect: ".mysql_error());
-    	$resultStr="connect fail";
-    }else{
-        //2)、选择数据库（目标）
-	    mysql_select_db("xianggangshasha",$conn);
-	    
-	    //3)、执行SQL语句（添加语句）（运输）
-	    //3.1)、查询该用户名是否被注册；
-	    
-	    $sqlstr = "select * from mydate where username= '".$username."'";
-	    $result = mysql_query($sqlstr,$conn);//查询语句的返回值是表格
-	    $rows = mysql_num_rows($result);//查询表格中有多少行
-	    if($rows>0){//说明该用户名在数据库中是存在的。
-	    	$resultStr="the username exists";	
-	    }else{
-		    $sqlstr = "insert into mydate values('".$username."','".$userpassword."','".$useremail."')";
-		    $str = mysql_query($sqlstr,$conn);
-		    if($str==1){
-			    $resultStr.href="index.html";
-			}else{
-				$resultStr="reg fail";
-			}
-	    }
-	    //4)、关闭数据库（拆桥）
-	    mysql_close($conn);
-	}
-	
-    //3、输出（响应）
- 	echo $resultStr;
-    
+	// 2、链接数据库，添加到数据库中
+	$conn = mysql_connect("localhost","root","root");
+	// (2)、选择数据库
+	mysql_select_db("xianggangshasha",$conn);
+	// (3)、执行插入语句
+	$sqlstr="insert into mydate(username,userpassword,useremail) values('".$username."','".$userpassword."','".$useremail."')";
+	mysql_query($sqlstr,$conn);
+	// (4)、关闭数据库
+	mysql_close($conn);
+	//3、响应（告诉前端，保存成功）
+	echo "注册成功";
 ?>
